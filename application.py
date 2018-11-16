51ff1866f534a092d9302542bf74eb15c9c4d341
Petra Barus <petra.barus@gmail.com>
2018-11-16 18:30:19 +0700

    Commit

--- application.py
+++ application.py
@@ -32,7 +32,7 @@ welcome = """
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Hello World!!</h1>
         <pre>addition({}, {}) = {}</pre>
     </body>
 </html>
